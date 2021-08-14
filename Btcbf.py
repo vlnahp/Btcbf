792643fcffef6d8cf2e8099da1f02ce18136c6be
vlnahp <87664667+vlnahp@users.noreply.github.com>
2021-08-14 17:09:08 +0430

    Huge update!
    Made the code more beautiful and clean.
    The most important change is the speed. Up to 3x faster!!
    Friendly multiprocessing added.

--- Btcbf.py
+++ Btcbf.py
@@ -1,43 +1,67 @@
-from os import urandom
-from bitcoin import privkey_to_address
-from codecs import decode, encode
-from hashlib import sha256
-from base58 import b58encode
+from bit.crypto import ECPrivateKey
+from bit.format import bytes_to_wif, public_key_to_address
 import mmap
 from tqdm import tqdm
 from sys import stdout
+from multiprocessing import Pool, cpu_count
 
-def gen(): 
-      for a in tqdm(range(int(10e15))):
-            pass
-            a = urandom(32).hex()
-            padding = '80' + a
-            # print( padding ) 
-            hashedVal = sha256(decode(padding, 'hex')).hexdigest()
-            checksum = sha256(decode(hashedVal, 'hex')).hexdigest()[:8]
-            # print( hashedVal )
-            # print( padding+checksum )
-            payload = padding + checksum
-            wif = b58encode(decode(payload, 'hex'))
-            #print(wif.decode('utf-8'))
-            z = privkey_to_address(wif.decode("utf-8")).encode("utf-8")
-            #print("Private Key: " + wif.decode('utf-8')) #Uncomment in order to use as address generater 
-            #print("public address: " + z.decode("utf-8")) #Uncomment in order to use as address generater 
-            with open('address.txt') as f:
+def generate_private_key():
+    private_key = ECPrivateKey()
+    return private_key
+    
+def generate_address():
+    address = public_key_to_address(generate_private_key().public_key.format())
+    return address
+
+def check_list(n):
+    n
+    z = generate_address().encode("utf-8")
+    wif = bytes_to_wif(generate_private_key().secret)
+    #print("Address: " + z.decode('utf-8')) #Uncomment in order to use as address generater
+    #print("Pravate key: " + wif) #Uncomment in order to use as address generater
+    with open('address.txt') as f:
                   s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ) # search for the generated address in the "address.txt" 
                   if s.find(z) != -1:
-                        print("true")
-                        print(a)
-                        print(z)
-                        print( wif.decode('utf-8') )
+                        print("Wow matching address found!!")
+                        print(z.decode('utf-8'))
+                        print(wif)
                         stdout = open("foundkey.txt", "w") # the found key and address saved to "foundkey.txt"
-                        print(a)
-                        print(z)
-                        print( wif.decode('utf-8') )
+                        print(z.decode('utf-8'))
+                        print(wif)
                         stdout.close()
                         exit()
                   #else: # uncommenting this part makes our code slow down
                         #print ('false')
                         #print(wif.decode("utf-8"))
                         #print(z)
-gen()
+
+
+def num_of_cores():
+    available_cores = cpu_count()
+    cores = input("How many cores to be used? (leave empty to use all available cores): ")
+    if cores == "":
+        return int(available_cores)
+    if cores.isdigit():
+        if 0 < int(cores) <= available_cores:
+            return int(cores)
+        if int(cores)<0 :
+            print("Hey you can't use negative number of cpu cores!!")
+            exit()
+        if int(cores) > available_cores:
+            print("Haha, you only have "+str(available_cores)+" cores. So we use "+str(available_cores)+" cores!!")
+            return int(available_cores)
+    else:
+        print("Wrong input!")
+        exit()
+
+def multiprocessing():
+    if __name__ == "__main__":
+        with Pool(processes=num_of_cores()) as pool:
+            #progress_bar = tqdm(total=10e10)
+            print("mapping ...")
+            results = tqdm(pool.imap_unordered(check_list, range(100000000000000000)), total=10e10)
+            print("running ...")
+            tuple(results)  # fetch the lazy results
+            print("done")
+
+multiprocessing()
