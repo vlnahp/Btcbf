d43c59288d0b4f2c9221c0336cecb6bcb973fd41
vlnahp <87664667+vlnahp@users.noreply.github.com>
2021-08-17 18:37:13 +0430

    Update Btcbf.py

--- Btcbf.py
+++ Btcbf.py
@@ -1,40 +1,37 @@
 from bit.crypto import ECPrivateKey
-from bit.format import bytes_to_wif, public_key_to_address
 import mmap
-from tqdm import tqdm
 from sys import stdout
 from multiprocessing import Pool, cpu_count
+from tqdm.tk import trange, tqdm, tqdm_tk
 from time import sleep
+from cryptotools.BTC import generate_keypair, push, script_to_address, OP
+private, public = generate_keypair()
+
+
+
 def generate_private_key():
     private_key = ECPrivateKey()
     return private_key
     
-def generate_address():
-    address = public_key_to_address(generate_private_key().public_key.format())
-    return address
-
 def check_list(n):
-    n
-    z = generate_address().encode("utf-8")
-    wif = bytes_to_wif(generate_private_key().secret)
-    #print("Address: " + z.decode('utf-8')) #Uncomment in order to use as address generater
-    #print("Pravate key: " + wif) #Uncomment in order to use as address generater
+    privkey = generate_private_key()
+    wif = private.wif(compressed=False) 
+    z = public.to_address('P2PKH').encode("utf-8") 
     with open('address.txt') as f:
-                  s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ) # search for the generated address in the "address.txt" 
-                  if s.find(z) != -1:
-                        print("Wow matching address found!!")
-                        print(z.decode('utf-8'))
-                        print(wif)
-                        stdout = open("foundkey.txt", "w") # the found key and address saved to "foundkey.txt"
-                        print(z.decode('utf-8'))
-                        print(wif)
-                        stdout.close()
-                        exit()
-                  #else: # uncommenting this part makes our code slow down
-                        #print ('false')
-                        #print(wif.decode("utf-8"))
-                        #print(z)
-
+        s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ) # search for the generated address in the "address.txt" 
+        if s.find(z) != -1:
+            print("Wow matching address found!!")
+            print(z.decode('utf-8'))
+            print(wif)
+            stdout = open("foundkey.txt", "w") # the found key and address saved to "foundkey.txt"
+            print(z.decode('utf-8'))
+            print(wif)
+            stdout.close()
+        #else: # uncommenting this part makes our code slow down
+            #print ('false')
+            #print(wif.decode("utf-8"))
+            #print(z)
+    n
 
 def num_of_cores():
     available_cores = cpu_count()
@@ -46,24 +43,44 @@ def num_of_cores():
             return int(cores)
         if int(cores)<=0 :
             print("Hey you can't use negative number of cpu cores!!")
-            sleep(8)
+            sleep(5)
             exit()
         if int(cores) > available_cores:
             print("Haha, you only have "+str(available_cores)+" cores. So we use "+str(available_cores)+" cores!!")
             return int(available_cores)
     else:
         print("Wrong input!")
+        print("exitting...")
         sleep(5)
         exit()
 
+def generate():
+    privkey = generate_private_key()
+    print("Public Address: "+public.to_address('P2PKH'))
+    print("Private Key: "+private.wif(compressed=False))
+
+
 def multiprocessing():
     if __name__ == "__main__":
+        inp = input("What do you want to do? <<options: [gen]: generate wallet address and private key, [brute]: brute force bitcoin, [exit]: exit>> ")
+        if inp == "gen":
+            generate()
+            print("Your wallet is ready!")
+            print("exitting...")
+            sleep(3)
+            exit()
+        if inp == "exit":
+            print("exitting")
+            sleep(5)
+            exit()
+        if inp == "brute":
+            pass
         with Pool(processes=num_of_cores()) as pool:
-            #progress_bar = tqdm(total=10e10)
-            print("mapping ...")
-            results = tqdm(pool.imap_unordered(check_list, range(100000000000000000)), total=10e10)
-            print("running ...")
-            tuple(results)  # fetch the lazy results
-            print("done")
-
+            r = range(100000000000000000)
+            print("Starting ...")
+            results = tqdm_tk(pool.imap_unordered(check_list, r), total=10e15)
+            print("Running ...")
+            tuple(results)
+            print("Stopping")
+            print()
 multiprocessing()
