f74725b9382e7c423ac1d0bb92a6b1f0f0bb209c
Nima <87664667+vlnahp@users.noreply.github.com>
2021-10-20 11:59:13 +0330

    Speed optimized
    up to 4x speed!

--- Btcbf.py
+++ Btcbf.py
@@ -1,13 +1,12 @@
 import requests
 import json
 from bit.crypto import ECPrivateKey
-import mmap
 from multiprocessing import Pool, cpu_count
 from tqdm import tqdm
 from time import sleep
 from bit.format import bytes_to_wif, public_key_to_address
 
-
+s= open("address.txt", "r").read()
 
 def generate_private_key():
     private_key = ECPrivateKey()
@@ -16,15 +15,13 @@ def generate_private_key():
 def check_list(n):
     privkey = generate_private_key()
     wif = bytes_to_wif(privkey.secret, compressed=True)
-    z = public_key_to_address(privkey.public_key.format()).encode("utf-8")
-    with open('address.txt') as f:
-        s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ) # search for the generated address in the "address.txt" 
-        if s.find(z) != -1:
+    z = public_key_to_address(privkey.public_key.format())
+    if s.find(z) != -1:
             print("Wow matching address found!!")
-            print(z.decode('utf-8'))
+            print(z)
             print(wif)
             f = open("foundkey.txt", "a") # the found key and address saved to "foundkey.txt"
-            f.write(z.decode('utf-8'))
+            f.write(z)
             f.write(wif)
             f.close()
         #else: # uncommenting this part makes our code slow down
@@ -56,14 +53,14 @@ def num_of_cores():
     cores = input("How many cores to be used? (leave empty to use all available cores): ")
     if cores == "":
         return int(available_cores)
-    if cores.isdigit():
+    elif cores.isdigit():
         if 0 < int(cores) <= available_cores:
             return int(cores)
-        if int(cores)<=0 :
+        elif int(cores)<=0 :
             print("Hey you can't use negative number of cpu cores!!")
             sleep(5)
             exit()
-        if int(cores) > available_cores:
+        elif int(cores) > available_cores:
             print("Haha, you only have "+str(available_cores)+" cores. So we use "+str(available_cores)+" cores!!")
             return int(available_cores)
     else:
@@ -85,20 +82,25 @@ def multiprocessing():
             generate()
             print("Your wallet is ready!")
             e = input("Press any key to exit")
-            if e:
-                exit()
-        if inp == "exit":
+            exit()
+        elif inp == "exit":
             print("exitting")
             sleep(5)
             exit()
-        if inp == "brute1":
+        elif inp == "brute1":
             target = check_list
             processn = num_of_cores()
-        if inp == "brute2":
+        elif inp == "brute2":
             print("OK I will consume whole your internet")
             target = check_list_online
             processn = num_of_cores()
-        with Pool(processes=num_of_cores()) as pool:
+        else:
+            print("No input. <gen> chosen automatically")
+            generate()
+            print("Your wallet is ready!")
+            f = input("Press any key to exit")
+            exit()
+        with Pool(processes=processn) as pool:
             r = range(100000000000000000)
             print("Starting ...")
             results = tqdm(pool.imap_unordered(target, r), total=10e15)
