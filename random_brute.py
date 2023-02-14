25504e92e7a9cf524ff303ecc4ce679fce290ea7
g0thic <mustafa.x@Msn.com>
2023-02-14 09:12:28 +0200

    Update random_brute.py

--- random_brute.py
+++ random_brute.py
@@ -233,7 +233,7 @@ class brute_manager():
     def run(self):
         try:
             self.ProcessList = list()
-            self.shared_ =  Multi_processing.Manager().list()
+            self.shared_ = Multi_processing.Manager().list()
             self.shared_.append(True)
             self.start_worker(workers=10,use_proxy=True)
         except BaseException as ex:
